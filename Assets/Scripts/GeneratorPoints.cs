2826d2190c8918e20fd17eef9c8c1d8d0158fe73
Artem314159 <recruter@i.ua>
2018-08-20 20:59:59 +0300

    Publish

---
+++ Assets/Scripts/GeneratorPoints.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public static class GeneratorPoints
+    {
+        public static float MinX = -9;
+        public static float MaxX = 9;
+        public static float MinZ = -9;
+        public static float MaxZ = 9;
+
+        public static List<Vector3> GetPointsCoords(int count, Vector3 playerPos)
+        {
+            int[] borders = { Convert.ToInt32(Math.Ceiling(MinX + 0.5f)), Convert.ToInt32(Math.Floor(MaxX - 0.5f)),
+                Convert.ToInt32(Math.Ceiling(MinZ + 0.5f)), Convert.ToInt32(Math.Floor(MaxZ - 0.5f)) };
+
+            List<Vector3> result = new List<Vector3>();
+            for (int i = 0; i < count; i++)
+            {
+                int randX = UnityEngine.Random.Range(borders[0], borders[1]);
+                int randZ = UnityEngine.Random.Range(borders[2], borders[3]);
+                Vector3 newVector = new Vector3(randX, 0, randZ);
+                if (!result.Contains(newVector) && !AreCrossing(playerPos, newVector))
+                {
+                    result.Add(newVector);
+                }
+                else i--;
+            }
+            return result;
+        }
+
+        private static bool AreCrossing(Vector3 first, Vector3 second)
+        {
+            return ((first.x < second.x + 2) && (first.x > second.x - 2)) && 
+                ((first.z < second.z + 2) && (first.z > second.z - 2));
+        }
+    }
+
+    public static class PoolPoints
+    {
+        public static Stack<GameObject> NonActive = new Stack<GameObject>();
+        public static GameObject Prefab = Resources.Load("Prefabs/Point") as GameObject;
+        private static int baseAmount = 10;
+
+        public static GameObject Pop()
+        {
+            GameObject newObj;
+            if (NonActive.Count == 0)
+                newObj = UnityEngine.Object.Instantiate(Prefab);
+            else newObj = NonActive.Pop();
+            newObj.SetActive(true);
+            return newObj;
+        }
+        public static void Push(GameObject newObj)
+        {
+            newObj.SetActive(false);
+            NonActive.Push(newObj);
+        }
+
+        public static void PoolInitialization()
+        {
+            for (int i = 0; i < baseAmount; i++)
+            {
+                Push(UnityEngine.Object.Instantiate(Prefab));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerComponent.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerComponent : MonoBehaviour
+{
+    public float speed = 5;
+    public int countPoints = 10;
+    public Canvas StartCanvas;
+    public Text ScoreText;
+    public Text WinText;
+
+    private Rigidbody rb;
+    private List<GameObject> Points;
+    private int score = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Assets.Scripts.PoolPoints.PoolInitialization();
+        WinText.enabled = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (!StartCanvas.enabled)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+            rb.AddForce(movement * speed);
+        }
+    }
+
+    public void ClickStartButton()
+    {
+        StartCanvas.enabled = false;
+        List<Vector3> pointsCoords = Assets.Scripts.GeneratorPoints.GetPointsCoords(countPoints, transform.position);
+        Points = new List<GameObject>();
+        for (int i = 0; i < countPoints; i++)
+        {
+            GameObject newPoint = Assets.Scripts.PoolPoints.Pop();
+            newPoint.transform.position = pointsCoords[i];
+            Points.Add(newPoint);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Point"))
+        {
+            Points.Remove(other.gameObject);
+            Assets.Scripts.PoolPoints.Push(other.gameObject);
+            score++;
+            ScoreText.text = "Score: " + score;
+        }
+        if (score == countPoints)
+            WinText.enabled = true;
+    }
+}
